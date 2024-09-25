a9cf8d32474da37c71aadadf60c3b9c3fd9741c5
KennySerpa <kserpa@me.com>
2024-09-24 21:48:50 -0700

    revert script again

--- deploy.sh
+++ deploy.sh
@@ -8,12 +8,10 @@ scp target/javaAzureWebApp.war apache:/tmp/
 
 # SSH into the server and perform deployment steps
 ssh apache << EOF
-  sudo -n chown tomcat:tomcat /tmp/javaAzureWebApp.war
-  sudo -n chmod 640 /tmp/javaAzureWebApp.war
-  sudo -n mv /tmp/javaAzureWebApp.war /opt/tomcat/webapps/
-  sudo -n systemctl restart tomcat
+  sudo chown tomcat:tomcat /tmp/javaAzureWebApp.war
+  sudo chmod 640 /tmp/javaAzureWebApp.war
+  sudo mv /tmp/javaAzureWebApp.war /opt/tomcat/webapps/
+  sudo systemctl restart tomcat
 EOF
 
 echo "Deployment completed successfully!"
-
-kserpa ALL=(ALL) NOPASSWD: /bin/chown tomcat:tomcat /tmp/javaAzureWebApp.war, /bin/chmod 640 /tmp/javaAzureWebApp.war, /bin/mv /tmp/javaAzureWebApp.war /opt/tomcat/webapps/, /bin/systemctl restart tomcat
